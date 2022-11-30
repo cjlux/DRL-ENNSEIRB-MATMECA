a32b5ae42026a9e1f089d300a5a80a219e373d91
Jean-Luc Charles <jean-luc.charles@ensam.eu>
2022-12-01 00:37:27 +0100

    Update JLC

--- TP-DRL/rewards.py
+++ TP-DRL/rewards.py
@@ -1,8 +1,3 @@
-###########################################################################
-# Copyright 2022 Jean-Luc CHARLES
-# Date of creation: 2022-07-29
-# License         : GNU GPL-3.0-or-later
-###########################################################################
 import numpy as np
 from numpy.linalg import norm
 from math import exp, pi
@@ -12,17 +7,6 @@ def reward(self, action):
     return 1
 
 def reward_0(self, action):
-        
-    q1, q1_dot, q2, q2_dot, x_tg, z_tg, x_ee, z_ee = self.state
-        
-    reward = 0
-	
-    # compute the reward....
-	
-    return reward
-
-    
-def reward_1(self, action):
         
     q1, q1_dot, q2, q2_dot, x_tg, z_tg, x_ee, z_ee = self.state
         
